29354b828d5e02b1b09dd407623f6087c296ad19
Robert Węglarek <weglarek.robert@gmail.com>
2020-07-02 01:18:51 +0200

    add total population annd area

--- insee/apps/api/places/serializers/region.py
+++ insee/apps/api/places/serializers/region.py
@@ -1,9 +1,23 @@
+from django.db.models import Sum
 from rest_framework import serializers
 
-from insee.apps.places.models import Region
+from insee.apps.places.models import City, Region
 
 
 class RegionSerializer(serializers.ModelSerializer):
+    totalArea = serializers.SerializerMethodField()
+    totalPopulation = serializers.SerializerMethodField()
+
     class Meta:
         model = Region
-        fields = ["code", "name"]
+        fields = ["code", "name", "totalArea", "totalPopulation"]
+
+    def get_totalArea(self, obj: Region):
+        return City.objects.filter(county__region=obj).aggregate(
+            total=Sum("area")
+        )["total"]
+
+    def get_totalPopulation(self, obj: Region):
+        return City.objects.filter(county__region=obj).aggregate(
+            total=Sum("population")
+        )["total"]
